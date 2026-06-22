bbb7c94c8309464e018f160022c463dbf0cd2ec4
Miniflint <tristan.goel@gmail.com>
2026-06-22 19:25:09 +0200

    mariadb instead of mysql

--- srcs/mariaDB/scripts/exec.sh
+++ srcs/mariaDB/scripts/exec.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-systemctl systemctl enable --now mariadb.service
-
-sleep 2
+systemctl enable mariadb.service
 
 echo "CREATE DATABASE IF NOT EXISTS $db1_name ;" > db1.sql
 echo "CREATE USER IF NOT EXISTS '$db1_user'@'%' IDENTIFIED BY '$db1_pwd' ;" >> db1.sql
@@ -9,8 +7,8 @@ echo "GRANT ALL PRIVILEGES ON $db1_name.* TO '$db1_user'@'%' ;" >> db1.sql
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
 echo "FLUSH PRIVILEGES;" >> db1.sql
 
-mysql < db1.sql
+mariadb < db1.sql
 
 kill $(cat /var/run/mysqld/mysqld.pid)
 
-mariadb
+mariadbd
